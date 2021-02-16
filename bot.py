1134c79efb2438ea8dd7f4958711d3ef3d1ac9c4
Nishant Sharma <42864292+goldenryu2000@users.noreply.github.com>
2021-02-16 13:56:37 +0530

    Updated bot.py
    The bot now does not rely on downloading the image on host machine and also not on OpenCV

--- bot.py
+++ bot.py
@@ -1,41 +1,33 @@
 import discord
 from discord.ext import commands
-import cv2
-import numpy as np
 import pytesseract
 from PIL import Image
+from PIL import ImageFilter
+from io import BytesIO
 import requests
 
-def get_string(img_path):
-    img = cv2.imread(img_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    kernel = np.ones((1, 1), np.uint8)
-    img = cv2.dilate(img, kernel, iterations=1)
-    img = cv2.erode(img, kernel, iterations=1)
-    cv2.imwrite("removed_noise.png", img)
-    cv2.imwrite(img_path, img)
-    result = pytesseract.image_to_string(Image.open(img_path))
-    return result
+def process_image(url):
+    image = _get_image(url)
+    image.filter(ImageFilter.SHARPEN)
+    return pytesseract.image_to_string(image)
 
+def _get_image(url):
+    return Image.open(BytesIO(requests.get(url).content))
 
 bot = commands.Bot(command_prefix='.', description="This is an OCR-Butt")
 # Events
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Streaming(name="Butts", url="abcd"))
-    print("Lessa GO!! It's Butt Time!!!!") # Change this to whatever you Like.
+    await bot.change_presence(activity=discord.Streaming(name="Butts", url="https://www.youtube.com/watch?v=sy9ztJuZu2c"))
+    print("Lessa GO!! It's Butt Time!!!!")
 
 @bot.event
 async def on_message(message):
     try:
         url = message.attachments[0].url
-        r = requests.get(url)
-        filename = "img.png"
-        with open(filename, 'wb') as out_file:
-            out_file.write(r.content)
         print(url)
         context = await bot.get_context(message)
-        ocr = get_string("img.png")
+        ocr = process_image(url)
         try:
             await context.send(ocr)
         except:
@@ -44,5 +36,5 @@ async def on_message(message):
         pass
     await bot.process_commands(message)
     
-## Add your Bot Token Here.    
-bot.run(BOT_TOKEN)
+  #Add your BOT TOKEN here:   
+bot.run('BOT_TOKEN')
